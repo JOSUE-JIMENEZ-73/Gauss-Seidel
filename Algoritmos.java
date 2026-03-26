342ee5286b465b3c0536d968f2fb15f4d3b939cd
JOSUE-JIMENEZ-73 <jimenezlaptop08@gmail.com>
2026-03-25 23:37:48 -0600

    comprobacion con la sustitucion

--- Algoritmos.java
+++ Algoritmos.java
@@ -1,36 +1,31 @@
 public class Algoritmos {
 
-    // Funcion auxiliar para reducir a 5 decimales
-    private double redondear(double valor) {
-        return Math.round(valor * 100000.0) / 100000.0;
-    }
-
     public String metodoGaussSeidel(double[][] A, double[] b, double tolerancia) {
         int n = b.length;
-        String salida = ""; 
-        
+        String salida = "";
+
         // Guardar copias exactas para la comprobacion al final
         double[][] matrizOriginal = new double[n][n];
         double[] vectorOriginal = new double[n];
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             vectorOriginal[i] = b[i];
-            for(int j = 0; j < n; j++) {
+            for (int j = 0; j < n; j++) {
                 matrizOriginal[i][j] = A[i][j];
             }
         }
 
-        salida += "Acomodando filas (Pivoteo Parcial)...\n";
+        salida += "Acomodando filas\n";
         for (int k = 0; k < n; k++) {
             int filaMayor = k;
             double maximo = Math.abs(A[k][k]);
-            
+
             for (int i = k + 1; i < n; i++) {
                 if (Math.abs(A[i][k]) > maximo) {
                     maximo = Math.abs(A[i][k]);
                     filaMayor = i;
                 }
             }
-            
+
             if (filaMayor != k) {
                 for (int j = 0; j < n; j++) {
                     double temp = A[k][j];
@@ -44,14 +39,14 @@ public class Algoritmos {
         }
 
         // Variables para iteracion de Gauss-Seidel
-        double[] x = new double[n]; 
+        double[] x = new double[n];
         int iteracion = 1;
         int limiteIteraciones = 1000;
         double errorMaximo;
 
         do {
             errorMaximo = 0.0;
-            
+
             for (int i = 0; i < n; i++) {
                 double suma = b[i];
                 for (int j = 0; j < n; j++) {
@@ -60,52 +55,76 @@ public class Algoritmos {
                         suma = redondear(suma - redondear(A[i][j] * x[j]));
                     }
                 }
-                
+
                 if (A[i][i] == 0) {
                     return salida + "Error: Un elemento en la diagonal es 0. El sistema no se puede resolver.";
                 }
-                
+
                 // Redondear el nuevo valor de la variable
                 double xNuevo = redondear(suma / A[i][i]);
-                
+
                 // Calcular el error y redondearlo
                 double errorActual = 0;
                 if (xNuevo != 0) {
-                    errorActual = redondear(Math.abs((xNuevo - x[i]) / xNuevo));
+                    errorActual = (xNuevo - x[i]) / xNuevo;
                 }
-                
+
                 if (errorActual > errorMaximo) {
                     errorMaximo = errorActual;
                 }
-                
-                x[i] = xNuevo; 
+
+                x[i] = xNuevo;
             }
             iteracion++;
-            
+
         } while (errorMaximo > tolerancia && iteracion <= limiteIteraciones);
 
         // Imprimir resultados a 5 decimales
         if (iteracion > limiteIteraciones) {
             salida += "Se llego al limite de iteraciones.\n";
         }
-        
+
         salida += "\nIteracion donde paro: " + (iteracion - 1) + "\n";
         salida += "Error maximo en que paro: " + errorMaximo + "\n\n";
-        
+
         salida += "Valores finales de X:\n";
         for (int i = 0; i < n; i++) {
             salida += "x_" + (i + 1) + " = " + redondear(x[i]) + "\n";
         }
 
-        salida += "\nComprobacion:\n";
+        salida += "\nComprobacion (Sustitucion en ecuaciones originales):\n";
+
         for (int i = 0; i < n; i++) {
             double comprobacion = 0;
+            String textoSustitucion = ""; 
+
             for (int j = 0; j < n; j++) {
-                comprobacion = redondear(comprobacion + redondear(matrizOriginal[i][j] * x[j]));
+                double coeficiente = matrizOriginal[i][j];
+                double valorX = redondear(x[j]);
+
+                //Suma matematica interna
+                comprobacion = redondear(comprobacion + redondear(coeficiente * valorX));
+
+                // Construccion del texto de sustitucion
+                textoSustitucion += "(" + coeficiente + ")(" + valorX + ")";
+
+                // Poner el signo "+" si no es el ultimo numero de la ecuacion
+                if (j < n - 1) {
+                    textoSustitucion += " + ";
+                }
             }
-            salida += "Fila " + (i + 1) + " calculada: " + comprobacion + " | Era: " + vectorOriginal[i] + "\n";
+
+            //Imprimir la linea completa
+            salida += "Fila " + (i + 1) + ": " + textoSustitucion + " = " + comprobacion + " | Era: "
+                    + vectorOriginal[i] + "\n";
         }
 
         return salida;
     }
+
+    // Funcion auxiliar para reducir a 5 decimales
+    private double redondear(double valor) {
+        return Math.round(valor * 100000.0) / 100000.0;
+    }
+
 }
